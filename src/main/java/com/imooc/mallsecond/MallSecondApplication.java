bfd8c4cf46c1f3c77111bfed452c04dcc42771e5
leedocode <1443124181@qq.com>
2023-05-07 20:40:37 +0800

    创建空项目，初次提交

---
+++ src/main/java/com/imooc/mallsecond/MallSecondApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.mallsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/mallsecond/MallSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.mallsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
