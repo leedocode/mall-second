6d953a63efd350033421d3eb68c046c70271e1d0
leedocode <1443124181@qq.com>
2023-05-09 12:02:44 +0800

    新增专门的test文件，增加mybatis-generator插件

--- src/test/java/com/imooc/mallsecond/MallSecondApplicationTests.java
+++ src/test/java/com/imooc/mallsecond/MallSecondApplicationTests.java
@@ -1,30 +1,10 @@
 package com.imooc.mallsecond;
 
-import com.imooc.mallsecond.dao.CategoryMapper;
-import com.imooc.mallsecond.pojo.Category;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallSecondApplicationTests {
-
-    @Autowired
-    private CategoryMapper categoryMapper;
-
-    @Test
-    public void testCategoryMapper() {
-        Category category = categoryMapper.findById(100001);
-        System.out.println(category.toString());
-    }
-
-    @Test
-    public void testQueryById() {
-        Category category = categoryMapper.queryById(100001);
-        System.out.println(category.toString());
-    }
-
 }
