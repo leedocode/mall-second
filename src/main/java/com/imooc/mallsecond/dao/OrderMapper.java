8de8deab572ffc7bcf1d6cf0831f618fd66d824f
leedocode <1443124181@qq.com>
2023-05-09 16:54:31 +0800

    配置Order相关的mapper文件 增加对OrderMapper接口的test

---
+++ src/main/java/com/imooc/mallsecond/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.mallsecond.dao;
+
+import com.imooc.mallsecond.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/test/java/com/imooc/mallsecond/dao/OrderMapperTest.java
@@ -0,0 +1,39 @@
+package com.imooc.mallsecond.dao;
+
+import com.imooc.mallsecond.MallSecondApplicationTests;
+import com.imooc.mallsecond.pojo.Order;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class OrderMapperTest extends MallSecondApplicationTests {
+
+    @Autowired
+    OrderMapper orderMapper;
+    @Test
+    public void deleteByPrimaryKey() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void insertSelective() {
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+        Order order = orderMapper.selectByPrimaryKey(1);
+        System.out.println(order.toString());
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() {
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+    }
+}
